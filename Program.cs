f557cdc78bd198b942d635a9d66966700bf63686
martcpp <mordecaiemmanueletukudo@gmail.com>
2023-01-14 12:47:58 +0100

    variable

--- Program.cs
+++ Program.cs
@@ -6,15 +6,13 @@ namespace mylearming
     {
         static void Main(string[] args)
         {
-            String a = "yh";
-            
-            Console.WriteLine(b);
-
-
-
-
-            Console.WriteLine("Hello \a World!");
-           
+            // how to declare variable in cshaarp
+            int num1; // use for interger postive or negative whole number e.g -2, -1 0 1 2 ......
+            double num2; // floating point number with long precision e.g 2.33648773838...
+            float num3; // floating point number with less precsision e.g 2.4335f ...
+            char b; // for specail character or one letter word e.g 'e' '\a'
+            string name; // to store long word or pharse
+            bool fact; // use to store Ture or false statement
         }
     }
 }
