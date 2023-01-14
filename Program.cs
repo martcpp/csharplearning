1a7012df8eeeaa85601aac3eb371cda468c9709d
martcpp <mordecaiemmanueletukudo@gmail.com>
2023-01-14 12:33:25 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace mylearming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a = "yh";
+            
+            Console.WriteLine(b);
+
+
+
+
+            Console.WriteLine("Hello \a World!");
+           
+        }
+    }
+}
